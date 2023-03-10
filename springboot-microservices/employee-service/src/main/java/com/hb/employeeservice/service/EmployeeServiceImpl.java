db4ec5344f13a13fac868d4ef02ba33d4fe1dc55
Hrishabh Kumar <hrishabh0007@gmail.com>
2023-03-10 23:28:47 +0530

    Rest API call using WebClient

--- springboot-microservices/employee-service/src/main/java/com/hb/employeeservice/service/EmployeeServiceImpl.java
+++ springboot-microservices/employee-service/src/main/java/com/hb/employeeservice/service/EmployeeServiceImpl.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
@@ -18,7 +19,8 @@ import java.util.Optional;
 public class EmployeeServiceImpl implements  EmployeeService{
 
     private EmployeeRepository employeeRepository;
-    private RestTemplate restTemplate;
+    //private RestTemplate restTemplate;
+    private  WebClient webClient;
     @Override
     public EmployeeDTO saveEmployee(EmployeeDTO employeeDTO) {
         Employee employee = new Employee(
@@ -47,10 +49,16 @@ public class EmployeeServiceImpl implements  EmployeeService{
 
         Employee employee = employeeRepository.findById(id).get();
 
-        ResponseEntity<DepartmentDTO> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
-                DepartmentDTO.class);
+//        ResponseEntity<DepartmentDTO> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+//                DepartmentDTO.class);
+//
+//        DepartmentDTO departmentDTO = responseEntity.getBody();
 
-        DepartmentDTO departmentDTO = responseEntity.getBody();
+        DepartmentDTO departmentDTO = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDTO.class)
+                .block();
 
         EmployeeDTO existingEmployee = new EmployeeDTO(employee.getId(),
                 employee.getFirstName(),
